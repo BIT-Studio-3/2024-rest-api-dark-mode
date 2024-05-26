a125160807c6f70c3ddc4016fd1a154c76d404c3
Navheen Pannir <navdx0508@gmail.com>
2024-05-26 18:51:35 +1200

    Added marketplace migration

---
+++ prisma/migrations/20240526063541_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `goods` on the `Market` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Market" DROP COLUMN "goods";
