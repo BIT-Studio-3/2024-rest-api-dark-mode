8ea264bdf6920a5add92957359ec089996f966b1
Navheen Pannir <navdx0508@gmail.com>
2024-06-06 23:36:20 +1200

    added seeding for market items

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,7 +3,6 @@ const prisma = new PrismaClient();
 
 const main = async () => {
   try {
-
     const alphaCentauri = await prisma.system.create({
       data: {
         name: "Alpha Centauri",
@@ -31,23 +30,112 @@ const main = async () => {
       },
     });
 
-  
     const waypoints = [
-      { name: "Waypoint 1", type: "ASTEROID", systemId: alphaCentauri.id, xCoordinate: 11.1, yCoordinate: 25.1 },
-      { name: "Waypoint 2", type: "ASTEROID", systemId: alphaCentauri.id, xCoordinate: 11.2, yCoordinate: 25.2 },
-      { name: "Waypoint 3", type: "MARKETPLACE", systemId: alphaCentauri.id, xCoordinate: 11.3, yCoordinate: 25.3 },
-      { name: "Waypoint 4", type: "SHIPYARD", systemId: alphaCentauri.id, xCoordinate: 11.4, yCoordinate: 25.4 },
-      { name: "Waypoint 5", type: "ASTEROID", systemId: alphaCentauri.id, xCoordinate: 11.5, yCoordinate: 25.5 },
-      { name: "Waypoint 1", type: "ASTEROID", systemId: betaProxima.id, xCoordinate: 11.1, yCoordinate: 25.1 },
-      { name: "Waypoint 2", type: "SHIPYARD", systemId: betaProxima.id, xCoordinate: 11.2, yCoordinate: 25.2 },
-      { name: "Waypoint 3", type: "ASTEROID", systemId: betaProxima.id, xCoordinate: 11.3, yCoordinate: 25.3 },
-      { name: "Waypoint 4", type: "ASTEROID", systemId: betaProxima.id, xCoordinate: 11.4, yCoordinate: 25.4 },
-      { name: "Waypoint 5", type: "ASTEROID", systemId: betaProxima.id, xCoordinate: 11.5, yCoordinate: 25.5 },
-      { name: "Waypoint 1", type: "ASTEROID", systemId: gammaHercules.id, xCoordinate: 11.1, yCoordinate: 25.1 },
-      { name: "Waypoint 2", type: "ASTEROID", systemId: gammaHercules.id, xCoordinate: 11.2, yCoordinate: 25.2 },
-      { name: "Waypoint 3", type: "ASTEROID", systemId: gammaHercules.id, xCoordinate: 11.3, yCoordinate: 25.3 },
-      { name: "Waypoint 4", type: "ASTEROID", systemId: gammaHercules.id, xCoordinate: 11.4, yCoordinate: 25.4 },
-      { name: "Waypoint 5", type: "ASTEROID", systemId: gammaHercules.id, xCoordinate: 11.5, yCoordinate: 25.5 },
+      {
+        name: "Waypoint 1",
+        type: "ASTEROID",
+        systemId: alphaCentauri.id,
+        xCoordinate: 11.1,
+        yCoordinate: 25.1,
+      },
+      {
+        name: "Waypoint 2",
+        type: "ASTEROID",
+        systemId: alphaCentauri.id,
+        xCoordinate: 11.2,
+        yCoordinate: 25.2,
+      },
+      {
+        name: "Waypoint 3",
+        type: "MARKETPLACE",
+        systemId: alphaCentauri.id,
+        xCoordinate: 11.3,
+        yCoordinate: 25.3,
+      },
+      {
+        name: "Waypoint 4",
+        type: "SHIPYARD",
+        systemId: alphaCentauri.id,
+        xCoordinate: 11.4,
+        yCoordinate: 25.4,
+      },
+      {
+        name: "Waypoint 5",
+        type: "ASTEROID",
+        systemId: alphaCentauri.id,
+        xCoordinate: 11.5,
+        yCoordinate: 25.5,
+      },
+      {
+        name: "Waypoint 1",
+        type: "ASTEROID",
+        systemId: betaProxima.id,
+        xCoordinate: 11.1,
+        yCoordinate: 25.1,
+      },
+      {
+        name: "Waypoint 2",
+        type: "SHIPYARD",
+        systemId: betaProxima.id,
+        xCoordinate: 11.2,
+        yCoordinate: 25.2,
+      },
+      {
+        name: "Waypoint 3",
+        type: "ASTEROID",
+        systemId: betaProxima.id,
+        xCoordinate: 11.3,
+        yCoordinate: 25.3,
+      },
+      {
+        name: "Waypoint 4",
+        type: "ASTEROID",
+        systemId: betaProxima.id,
+        xCoordinate: 11.4,
+        yCoordinate: 25.4,
+      },
+      {
+        name: "Waypoint 5",
+        type: "ASTEROID",
+        systemId: betaProxima.id,
+        xCoordinate: 11.5,
+        yCoordinate: 25.5,
+      },
+      {
+        name: "Waypoint 1",
+        type: "ASTEROID",
+        systemId: gammaHercules.id,
+        xCoordinate: 11.1,
+        yCoordinate: 25.1,
+      },
+      {
+        name: "Waypoint 2",
+        type: "ASTEROID",
+        systemId: gammaHercules.id,
+        xCoordinate: 11.2,
+        yCoordinate: 25.2,
+      },
+      {
+        name: "Waypoint 3",
+        type: "ASTEROID",
+        systemId: gammaHercules.id,
+        xCoordinate: 11.3,
+        yCoordinate: 25.3,
+      },
+      {
+        name: "Waypoint 4",
+        type: "ASTEROID",
+        systemId: gammaHercules.id,
+        xCoordinate: 11.4,
+        yCoordinate: 25.4,
+      },
+      {
+        name: "Waypoint 5",
+        type: "ASTEROID",
+        systemId: gammaHercules.id,
+        xCoordinate: 11.5,
+        yCoordinate: 25.5,
+      },
     ];
 
     for (const waypoint of waypoints) {
@@ -56,20 +144,63 @@ const main = async () => {
       });
 
       if (waypoint.type === "MARKETPLACE") {
-        await prisma.market.create({
+        const market = await prisma.market.create({
           data: {
             name: `${waypoint.name} Market`,
-            description: "A bustling hub for trade goods that are available for purchase which can be viewed",
+            description:
+              "A bustling hub for trade goods that are available for purchase which can be viewed",
             xCoordinate: waypoint.xCoordinate,
             yCoordinate: waypoint.yCoordinate,
             waypointId: createdWaypoint.id,
+          },
+        });
+
+        await prisma.marketItem.create({
+          data: {
+            name: "Iron Ore",
+            price: 50.0,
+            quantity: 1000,
+            marketId: market.id,
+          },
+        });
+        await prisma.marketItem.create({
+          data: {
+            name: "Silver Ore",
+            price: 500.0,
+            quantity: 500,
+            marketId: market.id,
+          },
+        });
+        await prisma.marketItem.create({
+          data: {
+            name: "Gold Ore",
+            price: 1000.0,
+            quantity: 100,
+            marketId: market.id,
+          },
+        });
+        await prisma.marketItem.create({
+          data: {
+            name: "Platinum Ore",
+            price: 2000.0,
+            quantity: 50,
+            marketId: market.id,
+          },
+        });
+        await prisma.marketItem.create({
+          data: {
+            name: "Diamond Ore",
+            price: 5000.0,
+            quantity: 10,
+            marketId: market.id,
           },
         });
       } else if (waypoint.type === "SHIPYARD") {
         await prisma.shipyard.create({
           data: {
             name: `${waypoint.name} Shipyard`,
-            description: "A shipyard where ships within the system are available for purchase",
+            description:
+              "A shipyard where ships within the system are available for purchase",
             xCoordinate: waypoint.xCoordinate,
             yCoordinate: waypoint.yCoordinate,
             waypointId: createdWaypoint.id,
@@ -78,7 +209,9 @@ const main = async () => {
       }
     }
 
-    console.log("Systems, waypoints, markets, and shipyards seeded successfully.");
+    console.log(
+      "Systems, waypoints, markets, and shipyards seeded successfully."
+    );
   } catch (err) {
     console.error("Error seeding data:", err);
   } finally {
